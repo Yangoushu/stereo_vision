f8fdb7fa7dadf50b1d3c62317fdeea8a05158694
Prasad Dalavi <prasad01dalavi@gmail.com>
2018-03-10 16:47:06 +0530

    initial commit

---
+++ disparity_map_snippet.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+imgL = cv2.imread('left_1.ppm',0)
+imgR = cv2.imread('right_1.ppm',0)
+# stereo = cv2.createStereoBM(numDisparities=16, blockSize=15)
+stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=16, SADWindowSize=15)
+disparity = stereo.compute(imgL,imgR)
+plt.imshow(disparity,'gray')
+plt.show()
